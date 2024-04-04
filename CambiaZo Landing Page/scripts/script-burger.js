c72c4fd97af97df0c5c1b78ef3fcb6f0238bb55c
cambiaz0 <165984250+cambiaz0@users.noreply.github.com>
2024-04-04 00:09:00 -0500

    Delete CambiaZo Landing Page directory

--- CambiaZo Landing Page/scripts/script-burger.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var burgerButton = document.querySelector('.burger-menu');
-    var navbar = document.querySelector('.navbar');
-  
-    burgerButton.addEventListener('click', function () {
-        navbar.classList.toggle('active');
-    });
-});
